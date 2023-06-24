7f8bfa7f52cfc572907ab6b83477b0e4dc7c82f9
Joe Price <joseph.price@missionlabs.co.uk>
2023-06-24 23:21:34 +0100

    feat: adds NavbarItem component

---
+++ src/components/Navbar/NavbarItem/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+import './navbar-item.scss';
+
+type Props = {
+  text: string;
+  link?: string;
+};
+
+function NavbarItem({ text, link = '/' }: Props) {
+  return (
+    <li className="navbar-item">
+      <Link to={link}>
+        <h2>{text}</h2>
+      </Link>
+    </li>
+  );
+}
+
+export default NavbarItem;
