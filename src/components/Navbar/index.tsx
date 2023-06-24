bfdde2e6f7766eba8583ddaa202c19782b3293a2
Joe Price <joseph.price@missionlabs.co.uk>
2023-06-24 23:21:49 +0100

    feat: adds Navbar component

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import Header from "../Header";
+import NavbarItem from "./NavbarItem";
+
+import "./navbar.scss";
+
+function Navbar() {
+  return (
+    <Header>
+      <ul className="navbar-links">
+        <NavbarItem link="/" text="To Do List App" />
+        <div className="navbar-links-right">
+            <NavbarItem link="todos/create" text="Create A To Do" />
+            <NavbarItem link="todos" text="View To Dos" />
+        </div>
+      </ul>
+    </Header>
+  );
+}
+
+export default Navbar;
