9233905256edd270c77331b1090474f00045203c
Joe Price <joe_price195@outlook.com>
2023-06-25 11:34:08 +0100

    chore: moves ui code into new app directory

---
+++ app/src/views/ViewToDos/index.tsx
@@ -0,0 +1,14 @@
+import "./view-to-dos.scss";
+
+function ViewToDos() {
+  return (
+    <main className="view-to-dos-container">
+      <div className="view-to-dos-container-left">Left</div>
+      <div className="view-to-dos-container-right">
+        
+      </div>
+    </main>
+  );
+}
+
+export default ViewToDos;
--- src/views/ViewToDos/index.tsx
+++
@@ -1,5 +0,0 @@
-function ViewToDos() {
-  return <h2>View To Dos</h2>;
-}
-
-export default ViewToDos;
